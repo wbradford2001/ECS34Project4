acad44493ead90921af5542ea7a6a38fea68409a
Ubuntu <ubuntu@ip-10-192-10-214.us-west-1.compute.internal>
2022-11-18 23:50:59 +0000

    AddVertex

--- testsrc/CDijkstraPathRouterTest.cpp
+++ testsrc/CDijkstraPathRouterTest.cpp
@@ -8,25 +8,40 @@
 #include "DijkstraPathRouter.h"
 
 TEST(CDijkstraPathRouterTest, SimpleTest){
-    CDijkstraPathRouter DijkstraPathRouter();
+    CDijkstraPathRouter DijkstraPathRouter;
 
 }
 
 TEST(CDijkstraPathRouterTest, VertexCount1){
+    CDijkstraPathRouter DijkstraPathRouter;
+    DijkstraPathRouter.AddVertex("tag1");
+    DijkstraPathRouter.AddVertex("tag2");
 
-    EXPECT_EQ(0,0);
+    EXPECT_EQ(2,DijkstraPathRouter.VertexCount());
 }
 
 TEST(CDijkstraPathRouterTest, VertexCount2){
+    CDijkstraPathRouter DijkstraPathRouter;
+    DijkstraPathRouter.AddVertex("tag1");
+    DijkstraPathRouter.AddVertex("tag2");
+    DijkstraPathRouter.AddVertex(2);
+    DijkstraPathRouter.AddVertex(134);
 
-    EXPECT_EQ(0,0);
+    EXPECT_EQ(4,DijkstraPathRouter.VertexCount());
 }
 TEST(CDijkstraPathRouterTest, AddVertex1){
-
+    CDijkstraPathRouter DijkstraPathRouter;
+    DijkstraPathRouter.AddVertex("tag1");
+    DijkstraPathRouter.AddVertex("tag2");
+    DijkstraPathRouter.AddVertex(2);
+    DijkstraPathRouter.AddVertex(134);
     EXPECT_EQ(0,0);
 }
 TEST(CDijkstraPathRouterTest, AddVertex2){
+    CDijkstraPathRouter DijkstraPathRouter;
 
+    DijkstraPathRouter.AddVertex(2);
+    DijkstraPathRouter.AddVertex(134);
     EXPECT_EQ(0,0);
 }
 TEST(CDijkstraPathRouterTest, GetVertexTag1){
