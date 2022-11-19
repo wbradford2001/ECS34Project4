23a1a71238e1454cc86834988e016c91f7ee6fa9
Ubuntu <ubuntu@ip-10-192-10-214.us-west-1.compute.internal>
2022-11-19 00:18:38 +0000

    GetVertexTag

--- testsrc/CDijkstraPathRouterTest.cpp
+++ testsrc/CDijkstraPathRouterTest.cpp
@@ -45,12 +45,37 @@ TEST(CDijkstraPathRouterTest, AddVertex2){
     EXPECT_EQ(0,0);
 }
 TEST(CDijkstraPathRouterTest, GetVertexTag1){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
 
-    EXPECT_EQ(0,0);
+    std::any tag1 = DijkstraPathRouter.GetVertexTag(tag1Index);
+    std::any tag2 = DijkstraPathRouter.GetVertexTag(tag2Index);
+
+
+    EXPECT_EQ("tag1",std::any_cast<const char*>(tag1));
+    EXPECT_EQ("tag2",std::any_cast<const char*>(tag2));
+ 
+    
+
 }
 TEST(CDijkstraPathRouterTest, GetVertexTag2){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
+    int tag3Index = DijkstraPathRouter.AddVertex(2);
+    int tag4Index = DijkstraPathRouter.AddVertex(3);    
 
-    EXPECT_EQ(0,0);
+    std::any tag1 = DijkstraPathRouter.GetVertexTag(tag1Index);
+    std::any tag2 = DijkstraPathRouter.GetVertexTag(tag2Index);
+    std::any tag3 = DijkstraPathRouter.GetVertexTag(tag3Index);
+    std::any tag4 = DijkstraPathRouter.GetVertexTag(tag4Index);    
+
+
+    EXPECT_EQ("tag1",std::any_cast<const char*>(tag1));
+    EXPECT_EQ("tag2",std::any_cast<const char*>(tag2));  
+    EXPECT_EQ(2,std::any_cast<int>(tag3));
+    EXPECT_EQ(3,std::any_cast<int>(tag4));     
 }
 TEST(CDijkstraPathRouterTest, AddEdge1){
 
