80896ea138dafd06682e22dcc90a3f0c8897158a
Ubuntu <ubuntu@ip-10-192-10-214.us-west-1.compute.internal>
2022-11-19 03:45:34 +0000

    FINDSHORTESTROUTE

--- src/CDijkstraPathRouter.cpp
+++ src/CDijkstraPathRouter.cpp
@@ -1,14 +1,27 @@
 #include "DijkstraPathRouter.h"
 #include "PathRouter.h"
 #include <unordered_map>
+#include <unordered_set>
+#include <limits.h>
+#include <bits/stdc++.h>
 struct CDijkstraPathRouter::SImplementation{
     struct Vertex{
             std::any tag;
+            int ID;
             std::vector< std::pair< std::shared_ptr<Vertex>, int > > Nexts;
         };    
 
     std::unordered_map< int, std::shared_ptr<Vertex> > VertexHash; //Hash that maps vertex ID's to Vertex Objects
     int ID = 0;//Start with ID of 1
+
+    std::vector<int> VList;//Vertex List
+
+    //Map from Source to a Map which contains the distance to all nodes
+    std::unordered_map<int, std::unordered_map<int, int> > VertexToPathLengths;
+
+
+    //Map from Source to a Map which contains the path to all nodes
+    std::unordered_map<int,  std::unordered_map<int, std::vector<int> > > VertexToPaths;
     SImplementation(){
     };
 
@@ -21,7 +34,10 @@ struct CDijkstraPathRouter::SImplementation{
         std::shared_ptr<Vertex> newVertex = std::make_unique<Vertex>();
         newVertex->tag = tag;
         ID += 1;
+        newVertex->ID = ID;
         VertexHash[ID] = newVertex;
+
+        VList.push_back(ID);
         return ID;
 
     };
@@ -34,10 +50,10 @@ struct CDijkstraPathRouter::SImplementation{
         return std::any();
     };
     bool AddEdge(TVertexID src, TVertexID dest, double weight, bool bidir) noexcept{
-    // Adds an edge between src and dest vertices with a weight of weight. If
-    // bidir is set to true an additional edge between dest and src is added. If
-    // src or dest nodes do not exist, or if the weight is negative the AddEdge
-    // will return false, otherwise it returns true.
+        // Adds an edge between src and dest vertices with a weight of weight. If
+        // bidir is set to true an additional edge between dest and src is added. If
+        // src or dest nodes do not exist, or if the weight is negative the AddEdge
+        // will return false, otherwise it returns true.
         if (!(VertexHash.at(src)) || !(VertexHash.at(dest)) || weight < 0){
             return false;
         }
@@ -45,23 +61,128 @@ struct CDijkstraPathRouter::SImplementation{
         newPair.first = VertexHash.at(dest); 
         newPair.second = weight;
         VertexHash.at(src)->Nexts.push_back(newPair);
+
+        if (bidir==true){
+            std::pair<std::shared_ptr<Vertex>, int> newPair;
+            newPair.first = VertexHash.at(src); 
+            newPair.second = weight;
+            VertexHash.at(dest)->Nexts.push_back(newPair);
+        }
         return true;
 
     };
 
     bool Precompute(std::chrono::steady_clock::time_point deadline) noexcept{
         // Allows the path router to do any desired precomputation up to the deadline
+    
 
-
-        //iterate through vertices, update adjacency matrix each time.
+        //compute Dijkstra's for every vertex
+        for (int i = 0; i < VList.size(); i ++){
+            
+            DijkStraAuxill(VList[i]);
+        }
+       
+        //print all paths
+        // for (int i = 0; i < VList.size(); i ++){
+        //     std::cout<<"Starting at Vertex: "<<VList[i]<<std::endl;
+        //     for (int j = 0; j < VList.size(); j++){
+        //         std::cout<<"From "<<VList[i]<<" to "<< VList[j]<<" = "<<VertexToPathLengths[VList[i]][VList[j]] <<std::endl;
+        //     }
+        // }
         return true;
     };
 
+
+    //Auxillary Dijkstra's Algorithm
+    void DijkStraAuxill(TVertexID src){
+        int n = VList.size();
+        
+        //distances[NodeID] = distance
+        std::unordered_map<int, int> distances;
+        std::unordered_map<int, std::vector<int> > paths;
+        
+        for (int i = 0; i<n; i++){
+            distances[VList[i]] = INT_MAX;
+            paths[VList[i]] = {};
+        };
+
+
+        //distance from source to itself is 0
+        distances[src] = 0;
+        paths[src] = {(int)src};
+
+        
+
+        std::unordered_set<int> already_seen_map; //If Vertex ID is in already_seen, we have finished processing it.
+        
+        //iterate until already_seen_map has all vertices
+        int count = 0;
+        while (already_seen_map.size()<n && count < n){
+            count += 1;
+
+            //find minimum distance vertex
+            int minDist = INT_MAX;
+            int minID = src;
+
+            for (int vI = 0; vI < n; vI += 1){
+                //check if vertex not in seen and has minimal distance
+                if (already_seen_map.count(VList[vI])==0 && distances[VList[vI]]<minDist){
+                    minDist = distances[VList[vI]];
+                    minID = VList[vI];
+                }
+            }
+
+            //Add minID to already seen map
+            already_seen_map.insert(minID);
+            //std::cout<<"Now processing Node: "<<minID<<std::endl;
+
+            //updated distance value of all vertices adjacent to minID
+            
+            //iterate through all adjacent vertices
+            std::vector< std::pair< std::shared_ptr<Vertex>, int > > Nexts = VertexHash[minID]->Nexts;
+            for (int adjVI = 0; adjVI < Nexts.size(); adjVI++){
+                
+                int adjacentID = Nexts[adjVI].first->ID;
+                
+                //if current distance from adjacent vertices is more than distance to srouce from minID plus weight of edge, udpate distance
+                if (distances[adjacentID] > (distances[minID] + Nexts[adjVI].second)){
+                    distances[adjacentID] = distances[minID] + Nexts[adjVI].second;
+                    paths[adjacentID].clear();
+                    //add paths leading up to minID
+                    for (int k = 0; k < paths[minID].size(); k ++){
+                        paths[adjacentID].push_back(paths[minID][k]);
+                    }
+                    paths[adjacentID].push_back(adjacentID);
+                }
+            }
+
+        }
+    //print distances
+    // for (int vI = 0; vI < n; vI ++){
+        
+    //     std::cout<<"Distance to vertex: "<<VList[vI]<<"= "<<distances[VList[vI]]<<std::endl;
+    //     for (int pI = 0; pI < paths[VList[vI]].size(); pI ++){
+    //         std::cout<<"   "<<paths[VList[vI]][pI]<<std::endl;
+    //     }
+    // }
+    VertexToPathLengths[src] = distances;
+    VertexToPaths[src] = paths;
+
+
+    }
     double FindShortestPath(TVertexID src, TVertexID dest, std::vector<TVertexID> &path) noexcept{
         // Returns the path distance of the path from src to dest, and fills out path
-    // with vertices. If no path exists NoPathExists is returned.
-        double myDub;
-        return myDub;
+        // with vertices. If no path exists NoPathExists is returned.
+        if (VertexToPathLengths[src][dest]==INT_MAX){
+            return NoPathExists;
+        };
+        
+        double pathLength = VertexToPathLengths[src][dest];
+        std::vector<int> pathIDs = VertexToPaths[src][dest];
+        for (int i=0; i<pathIDs.size(); i++){
+            path.push_back(pathIDs[i]);
+        }
+        return pathLength;
 
     };
 
--- testsrc/CDijkstraPathRouterTest.cpp
+++ testsrc/CDijkstraPathRouterTest.cpp
@@ -77,29 +77,281 @@ TEST(CDijkstraPathRouterTest, GetVertexTag2){
     EXPECT_EQ(2,std::any_cast<int>(tag3));
     EXPECT_EQ(3,std::any_cast<int>(tag4));     
 }
-TEST(CDijkstraPathRouterTest, AddEdge1){
+TEST(CDijkstraPathRouterTest, AddEdgeRegular){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
+    int tag3Index = DijkstraPathRouter.AddVertex("tag3");
+    int tag4Index = DijkstraPathRouter.AddVertex("tag4"); 
 
-    EXPECT_EQ(0,0);
+    DijkstraPathRouter.AddEdge(tag1Index, tag2Index, 4, false);
 }
-TEST(CDijkstraPathRouterTest, AddEdge2){
+TEST(CDijkstraPathRouterTest, AddEdgeRegular2){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
+    int tag3Index = DijkstraPathRouter.AddVertex("tag3");
+    int tag4Index = DijkstraPathRouter.AddVertex("tag4"); 
 
-    EXPECT_EQ(0,0);
+    DijkstraPathRouter.AddEdge(tag1Index, tag2Index, 4, false);
+    DijkstraPathRouter.AddEdge(tag1Index, tag3Index, 3, false);    
+    DijkstraPathRouter.AddEdge(tag2Index, tag3Index, 2, false);    
+    DijkstraPathRouter.AddEdge(tag4Index, tag1Index, 1, false);    
 }
-TEST(CDijkstraPathRouterTest, Precompute){
+TEST(CDijkstraPathRouterTest, AddEdgeBidir){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
+    int tag3Index = DijkstraPathRouter.AddVertex("tag3");
+    int tag4Index = DijkstraPathRouter.AddVertex("tag4"); 
 
-    EXPECT_EQ(0,0);
+    DijkstraPathRouter.AddEdge(tag1Index, tag2Index, 4, true);
+    DijkstraPathRouter.AddEdge(tag1Index, tag3Index, 3, true);    
+    DijkstraPathRouter.AddEdge(tag2Index, tag3Index, 2, true);    
+    DijkstraPathRouter.AddEdge(tag4Index, tag1Index, 1, true);    
 }
-TEST(CDijkstraPathRouterTest, FindShortestPath1){
+TEST(CDijkstraPathRouterTest, Precompute1){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
+    int tag3Index = DijkstraPathRouter.AddVertex("tag3");
+    int tag4Index = DijkstraPathRouter.AddVertex("tag4"); 
 
-    EXPECT_EQ(0,0);
+    DijkstraPathRouter.AddEdge(tag1Index, tag2Index, 4, true);
+    DijkstraPathRouter.AddEdge(tag1Index, tag3Index, 3, true);    
+    DijkstraPathRouter.AddEdge(tag2Index, tag3Index, 2, true);    
+    DijkstraPathRouter.AddEdge(tag4Index, tag1Index, 1, true);  
+    std::chrono::steady_clock::time_point t;
+    DijkstraPathRouter.Precompute(t);
+
+
 }
-TEST(CDijkstraPathRouterTest, FindShortestPath2){
 
-    EXPECT_EQ(0,0);
+TEST(CDijkstraPathRouterTest, Precompute2){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
+    int tag3Index = DijkstraPathRouter.AddVertex("tag3");
+    int tag4Index = DijkstraPathRouter.AddVertex("tag4"); 
+    int tag5Index = DijkstraPathRouter.AddVertex("tag5");
+    int tag6Index = DijkstraPathRouter.AddVertex("tag6");
+    int tag7Index = DijkstraPathRouter.AddVertex("tag7");
+    int tag8Index = DijkstraPathRouter.AddVertex("tag8");
+    int tag9Index = DijkstraPathRouter.AddVertex("tag9");     
+
+    DijkstraPathRouter.AddEdge(tag1Index, tag2Index, 4, true);
+    DijkstraPathRouter.AddEdge(tag1Index, tag8Index, 8, true); 
+    DijkstraPathRouter.AddEdge(tag2Index, tag8Index, 11, true); 
+    DijkstraPathRouter.AddEdge(tag2Index, tag3Index, 8, true); 
+    DijkstraPathRouter.AddEdge(tag8Index, tag7Index, 1, true); 
+    DijkstraPathRouter.AddEdge(tag8Index, tag9Index, 7, true); 
+    DijkstraPathRouter.AddEdge(tag3Index, tag9Index, 2, true); 
+    DijkstraPathRouter.AddEdge(tag9Index, tag7Index, 6, true); 
+    DijkstraPathRouter.AddEdge(tag3Index, tag6Index, 4, true); 
+    DijkstraPathRouter.AddEdge(tag3Index, tag4Index, 7, true);
+    DijkstraPathRouter.AddEdge(tag7Index, tag6Index, 2, true);
+    DijkstraPathRouter.AddEdge(tag4Index, tag6Index, 14, true);
+    DijkstraPathRouter.AddEdge(tag4Index, tag5Index, 9, true);
+    DijkstraPathRouter.AddEdge(tag5Index, tag6Index, 10, true);
+
+    std::chrono::steady_clock::time_point t;
+    DijkstraPathRouter.Precompute(t);    
 }
-TEST(CDijkstraPathRouterTest, FindShortestPath3){
 
-    EXPECT_EQ(0,0);
+TEST(CDijkstraPathRouterTest, Precompute3){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
+    int tag3Index = DijkstraPathRouter.AddVertex("tag3");
+    int tag4Index = DijkstraPathRouter.AddVertex("tag4"); 
+ 
+
+    DijkstraPathRouter.AddEdge(tag1Index, tag2Index, 1, false);
+    DijkstraPathRouter.AddEdge(tag2Index, tag3Index, 2, false); 
+    DijkstraPathRouter.AddEdge(tag3Index, tag4Index, 20, false); 
+    DijkstraPathRouter.AddEdge(tag1Index, tag4Index, 10, false); 
+
+
+    std::chrono::steady_clock::time_point t;
+    DijkstraPathRouter.Precompute(t);    
+}
+TEST(CDijkstraPathRouterTest, ShortestPath1){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
+    int tag3Index = DijkstraPathRouter.AddVertex("tag3");
+    int tag4Index = DijkstraPathRouter.AddVertex("tag4"); 
+
+    DijkstraPathRouter.AddEdge(tag1Index, tag2Index, 4, true);
+    DijkstraPathRouter.AddEdge(tag1Index, tag3Index, 3, true);    
+    DijkstraPathRouter.AddEdge(tag2Index, tag3Index, 2, true);    
+    DijkstraPathRouter.AddEdge(tag4Index, tag1Index, 1, true);  
+    std::chrono::steady_clock::time_point t;
+    DijkstraPathRouter.Precompute(t);
+
+    CPathRouter::TVertexID src;
+    CPathRouter::TVertexID dest;
+    std::vector<CPathRouter::TVertexID> path;
+
+
+
+    src = 1;
+    dest = 4;
+    path.clear();
+    EXPECT_EQ(1,DijkstraPathRouter.FindShortestPath(src, dest, path));
+    EXPECT_EQ(path.size(),2);
+    EXPECT_EQ(path[0],1);
+    EXPECT_EQ(path[1],4);
+
+
+
+    src = 4;
+    dest = 1;
+    path.clear();
+    EXPECT_EQ(1,DijkstraPathRouter.FindShortestPath(src, dest, path));
+    EXPECT_EQ(path.size(),2);
+    EXPECT_EQ(path[0],4);
+    EXPECT_EQ(path[1],1); 
+
+    src = 1;
+    dest = 2;
+    path.clear();
+    EXPECT_EQ(4,DijkstraPathRouter.FindShortestPath(src, dest, path));
+    EXPECT_EQ(path.size(),2);
+    EXPECT_EQ(path[0],1);
+    EXPECT_EQ(path[1],2);     
+
+
+    src = 2;
+    dest = 1;
+    path.clear();
+    EXPECT_EQ(4,DijkstraPathRouter.FindShortestPath(src, dest, path));
+    EXPECT_EQ(path.size(),2);
+    EXPECT_EQ(path[0],2);
+    EXPECT_EQ(path[1],1);  
+
+    src = 3;
+    dest = 2;
+    path.clear();
+    EXPECT_EQ(2,DijkstraPathRouter.FindShortestPath(src, dest, path));
+    EXPECT_EQ(path.size(),2);
+    EXPECT_EQ(path[0],3);
+    EXPECT_EQ(path[1],2);         
+
+
+
+
 }
 
+TEST(CDijkstraPathRouterTest, ShortestPath2){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
+    int tag3Index = DijkstraPathRouter.AddVertex("tag3");
+    int tag4Index = DijkstraPathRouter.AddVertex("tag4"); 
+    int tag5Index = DijkstraPathRouter.AddVertex("tag5");
+    int tag6Index = DijkstraPathRouter.AddVertex("tag6");
+    int tag7Index = DijkstraPathRouter.AddVertex("tag7");
+    int tag8Index = DijkstraPathRouter.AddVertex("tag8");
+    int tag9Index = DijkstraPathRouter.AddVertex("tag9");     
+
+    DijkstraPathRouter.AddEdge(tag1Index, tag2Index, 4, true);
+    DijkstraPathRouter.AddEdge(tag1Index, tag8Index, 8, true); 
+    DijkstraPathRouter.AddEdge(tag2Index, tag8Index, 11, true); 
+    DijkstraPathRouter.AddEdge(tag2Index, tag3Index, 8, true); 
+    DijkstraPathRouter.AddEdge(tag8Index, tag7Index, 1, true); 
+    DijkstraPathRouter.AddEdge(tag8Index, tag9Index, 7, true); 
+    DijkstraPathRouter.AddEdge(tag3Index, tag9Index, 2, true); 
+    DijkstraPathRouter.AddEdge(tag9Index, tag7Index, 6, true); 
+    DijkstraPathRouter.AddEdge(tag3Index, tag6Index, 4, true); 
+    DijkstraPathRouter.AddEdge(tag3Index, tag4Index, 7, true);
+    DijkstraPathRouter.AddEdge(tag7Index, tag6Index, 2, true);
+    DijkstraPathRouter.AddEdge(tag4Index, tag6Index, 14, true);
+    DijkstraPathRouter.AddEdge(tag4Index, tag5Index, 9, true);
+    DijkstraPathRouter.AddEdge(tag5Index, tag6Index, 10, true);
+
+    std::chrono::steady_clock::time_point t;
+    DijkstraPathRouter.Precompute(t);    
+
+
+    CPathRouter::TVertexID src;
+    CPathRouter::TVertexID dest;
+    std::vector<CPathRouter::TVertexID> path;
+
+
+
+    src = 1;
+    dest = 2;
+    path.clear();
+    EXPECT_EQ(4,DijkstraPathRouter.FindShortestPath(src, dest, path));
+    EXPECT_EQ(path.size(),2);
+    EXPECT_EQ(path[0],1);
+    EXPECT_EQ(path[1],2);   
+
+
+    src = 1;
+    dest = 3;
+    path.clear();
+    EXPECT_EQ(12,DijkstraPathRouter.FindShortestPath(src, dest, path));
+    EXPECT_EQ(path.size(),3);
+    EXPECT_EQ(path[0],1);
+    EXPECT_EQ(path[1],2);
+    EXPECT_EQ(path[2],3); 
+
+    src = 5;
+    dest = 3;
+    path.clear();
+    EXPECT_EQ(14,DijkstraPathRouter.FindShortestPath(src, dest, path));
+    EXPECT_EQ(path.size(),3);
+    EXPECT_EQ(path[0],5);
+    EXPECT_EQ(path[1],6);
+    EXPECT_EQ(path[2],3);          
+}
+
+TEST(CDijkstraPathRouterTest, ShortestPath3){
+    CDijkstraPathRouter DijkstraPathRouter;
+    int tag1Index = DijkstraPathRouter.AddVertex("tag1");
+    int tag2Index = DijkstraPathRouter.AddVertex("tag2");
+    int tag3Index = DijkstraPathRouter.AddVertex("tag3");
+    int tag4Index = DijkstraPathRouter.AddVertex("tag4"); 
+ 
+
+    DijkstraPathRouter.AddEdge(tag1Index, tag2Index, 1, false);
+    DijkstraPathRouter.AddEdge(tag2Index, tag3Index, 2, false); 
+    DijkstraPathRouter.AddEdge(tag3Index, tag4Index, 20, false); 
+    DijkstraPathRouter.AddEdge(tag1Index, tag4Index, 10, false); 
+
+
+    std::chrono::steady_clock::time_point t;
+    DijkstraPathRouter.Precompute(t);  
+
+
+    CPathRouter::TVertexID src;
+    CPathRouter::TVertexID dest;
+    std::vector<CPathRouter::TVertexID> path;
+
+
+    src = 1;
+    dest = 2;
+    path.clear();
+    EXPECT_EQ(1,DijkstraPathRouter.FindShortestPath(src, dest, path));
+    EXPECT_EQ(path.size(),2);
+    EXPECT_EQ(path[0],1);
+    EXPECT_EQ(path[1],2);   
+
+    src = 2;
+    dest = 3;
+    path.clear();
+    EXPECT_EQ(2,DijkstraPathRouter.FindShortestPath(src, dest, path));
+    EXPECT_EQ(path.size(),2);
+    EXPECT_EQ(path[0],2);
+    EXPECT_EQ(path[1],3);   
+
+    src = 3;
+    dest = 1;
+    path.clear();
+    
+    EXPECT_EQ(CPathRouter::NoPathExists, DijkstraPathRouter.FindShortestPath(src, dest, path));
+           
+}
 
